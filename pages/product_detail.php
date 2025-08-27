592ab13ab8338b5833bcfbbb3f829e7243d37ff7
CodeSleek00 <codesleekstudio@gmail.com>
2025-08-27 23:45:05 +0530

    Update product_detail.php

--- pages/product_detail.php
+++ pages/product_detail.php
@@ -13,7 +13,7 @@ $product = $result->fetch_assoc();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $product['product_name']; ?> - Product Details</title>
-  <link rel="stylesheet" href="product-style.css">
+  <link rel="stylesheet" href="product-style.css?v=2.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
